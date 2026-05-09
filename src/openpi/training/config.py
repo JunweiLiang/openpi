b4ffff2c2c1dbafd92f341d267761ee182896f55
Junwei Liang <junweiliang1114@gmail.com>
2026-05-09 10:04:24 +0800

    updated

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -20,6 +20,7 @@ import openpi.models.tokenizer as _tokenizer
 import openpi.policies.aloha_policy as aloha_policy
 import openpi.policies.droid_policy as droid_policy
 import openpi.policies.libero_policy as libero_policy
+import openpi.policies.g1_v1_policy as g1_v1_policy
 import openpi.shared.download as _download
 import openpi.shared.normalize as _normalize
 import openpi.training.droid_rlds_dataset as droid_rlds_dataset
